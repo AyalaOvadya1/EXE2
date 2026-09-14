fc82695daae9f694ad83eb0c9f32212cac30a8b5
AyalaOvadya1 <ayovadya@g.jct.ac.il>
2026-09-14 22:23:08 +0300

    Part 2

--- functions.py
+++ functions.py
@@ -33,3 +33,24 @@ time_imperative = end_imp - start_imp
 everything = reduce(lambda x,y : x+linear_func(y) , range(0, 10001) ,0)
 
 
+##2
+
+my_list = range(1, 1001)
+odds  = list(filter(lambda x: x%2==1, my_list))
+evens = list(filter(lambda x: x%2==0, my_list))
+
+#2.1
+# למבדה לזוגיים: מכפלת המצטבר באיבר הבא
+even_lambda = lambda x, y: x * y
+
+# למבדה לאי-זוגיים: חישוב ליניארי על המצטבר + האיבר הבא
+odd_lambda = lambda x, y: (x / 2 + 2) + y
+
+#2.2
+even_func = reduce(even_lambda, evens , 1)
+odd_func = reduce(odd_lambda, odds , 0)
+
+#2.3
+final_sum = reduce(lambda x,y : x+y , [even_func,int(odd_func)])
+
+
